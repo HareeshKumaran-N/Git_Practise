3c908ea89d2a1e923131113fad87c26545d4f3d5
Hareesh K <nhareeshkumaran19cs@srishakthi.ac.in>
2022-12-06 00:56:23 +0530

    Edge cases are handled

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+/*
+
+Edge Cases are Handled in this version
+
+ */
+
+
 function reverse(str)
 {
+   if(typeof str!=='string' || str.length===0)
+   return "Invalid Input";
+   
    return str.split("").reverse().join("")
 }
 
-console.log(reverse("Hareesh"));
+console.log(reverse(""));
