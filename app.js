e2903cf3f548145d1191eeae1b2cc8ea8d70e003
Hareesh Kumaran Navaneethakrishnan <hareeshkumaran.navaneethakrishnan@cesltd.com>
2022-12-07 20:11:06 +0530

    palindrome function with separate reverse function

--- app.js
+++ app.js
@@ -2,26 +2,28 @@
 
 Functionalities:
 
->> Edge Cases are Handled.
-   1. if the input is said to be a empty string or if it is not a string the function wont work.
-
->> Palindrome functionality is added.
+>>Palindrome functionality is added but the reverse is written as a separate function.
 
 
 
  */
 
-
 function reverse(str)
-{
-   if(typeof str!=='string' || str.length===0)
-   return "Invalid Input";
-   
+{ 
    let revStr=(str.split("").reverse().join("") );
-   if(revStr===str)
-   return "Given string is a Palindrome: " + revStr;
+   console.log(revStr)
+    return (revStr);
+}
+function Palindrome(str)
+{console.log(str)
+
+   if(reverse(str)===str)
+   return "Palindrome";
    else
-   return "Not a Palindrome but the revString is  "+revStr;
+   return "Not a Palindrome";
 }
 
-console.log(reverse("malaalam"));
+
+
+
+console.log(Palindrome("mox"));
